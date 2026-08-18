8b4f4a16372a662b262dfa1aa090c5031a77e31c
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2026-08-18 17:40:11 +0600

    added biconnected inputs to compare unsuccessful checks

---
+++ one-connected-code-test-on-biconnected/a.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include "ParvezRahmanNakano.hpp"
+#define input(arr, n)          \
+    for (ll i = 0; i < n; i++) \
+        cin >> arr[i];
+#define output(arr, n)             \
+    {                              \
+        for (ll i = 0; i < n; i++) \
+        {                          \
+            cout << arr[i] << " "; \
+        }                          \
+    }
+using namespace std;
+typedef long long ll;
+// policy based data structure
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T>
+using indexed_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// for printing normal 1D vector
+template <typename T>
+void printVector(vector<T> &v, ll st = -1, ll fin = -1)
+{
+    if (st == -1)
+        st = 0;
+    if (fin == -1)
+        fin = v.size();
+    for (ll i = st; i < fin; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return;
+}
+
+template <typename T1, typename T2>
+void printVectorPair(vector<pair<T1, T2>> &v, ll st = -1, ll fin = -1)
+{
+    if (st == -1)
+        st = 0;
+    if (fin == -1)
+        fin = v.size();
+    for (ll i = st; i < fin; i++)
+    {
+        cout << "( " << v[i].first << " , " << v[i].second << " ) ";
+    }
+    cout << endl;
+    return;
+}
+
+// for printing 2D vectors (matrix)
+template <typename T>
+void printMatrix(vector<T> &v, ll st = -1, ll fin = -1)
+{
+    if (st == -1)
+        st = 0;
+    if (fin == -1)
+        fin = v.size();
+    for (ll i = st; i < fin; i++)
+    {
+        printVector(v[i]);
+    }
+    // cout << endl;
+    return;
+}
+
+// for printing other STL structures
+template <typename T>
+void printStructure(T &t)
+{
+    for (auto itr = t.begin(); itr != t.end(); itr++)
+    {
+        cout << *itr << " ";
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+
+    ParvezRahmanNakano *parvezRahmanNakano = new ParvezRahmanNakano(4);
+    parvezRahmanNakano->generateAllTriangulations();
+    parvezRahmanNakano->printAllTriangulations();
+
+    return 0;
+}
---
+++ one-connected-code-test-on-biconnected/triconnected.hpp
@@ -0,0 +1,190 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma once
+#include "Edge.hpp"
+#include "pairHash.hpp"
+#include "FaceTriangulation.hpp"
+#include "ParvezRahmanNakano.hpp"
+
+class triconnected
+{
+public:
+    vector<vector<int>> faces;
+    vector<vector<pair<int, int>>> allTriangulations;
+    vector<vector<vector<pair<int, int>>>> triangulation_per_face;
+    unordered_multiset<pair<int, int>, PairHash> present;
+    triconnected(vector<vector<int>> &faces)
+    {
+        this->faces = faces;
+
+        present = unordered_multiset<pair<int, int>, PairHash>();
+        initiatePresent();
+        triangulation_per_face = vector<vector<vector<pair<int, int>>>>(faces.size());
+    }
+    void initiatePresent()
+    {
+        for (auto face : faces)
+        {
+            for (int i = 0; i < face.size() - 1; i++)
+            {
+                present.insert(make_pair(min(face[i], face[i + 1]), max(face[i], face[i + 1])));
+            }
+            present.insert(make_pair(min(face[0], face[face.size() - 1]), max(face[0], face[face.size() - 1])));
+        }
+    }
+
+    /// @brief Generates all triangulations for each face and stores them in triangulation_per_face
+    void getAllTriangulations()
+    {
+        ParvezRahmanNakano *ft;
+        int pos = 0;
+        for (auto &face : faces)
+        {
+            int n = face.size();
+            ft = new ParvezRahmanNakano(n);
+            ft->generateAllTriangulations();
+            for (auto &triangulation : ft->allTriangulations)
+            {
+                vector<pair<int, int>> mappedTriangulation;
+                for (auto &chord : triangulation)
+                {
+                    // cout << "Chord in triangulation: (" << chord.first << ", " << chord.second << ")" << endl;
+                    mappedTriangulation.push_back({min(face[chord.first], face[chord.second]), max(face[chord.first], face[chord.second])});
+                }
+                sort(mappedTriangulation.begin(), mappedTriangulation.end());
+                triangulation_per_face[pos].push_back(mappedTriangulation);
+            }
+            // cout << "Face " << pos << " has " << triangulation_per_face[pos].size() << " triangulations." << endl;
+            delete ft;
+            pos++;
+        }
+    }
+
+    /// @brief Combines triangulations from different faces
+    void combineTriangulations(
+        const vector<vector<vector<pair<int, int>>>> &triangulation_per_face,
+        int index,
+        vector<pair<int, int>> &current,
+        vector<vector<pair<int, int>>> &allTriangulations)
+    {
+        // Skip faces with 0 triangulations
+        while (index < (int)triangulation_per_face.size() &&
+               triangulation_per_face[index].empty())
+        {
+            index++;
+        }
+
+        // Base case: all faces processed
+        if (index == (int)triangulation_per_face.size())
+        {
+            allTriangulations.push_back(current);
+            return;
+        }
+
+        // Recursive case: combine current triangulation with next face's triangulations
+        for (const auto &tri : triangulation_per_face[index])
+        {
+            size_t oldSize = current.size();
+            current.insert(current.end(), tri.begin(), tri.end());
+            combineTriangulations(triangulation_per_face, index + 1, current, allTriangulations);
+            current.resize(oldSize); // backtrack efficiently
+        }
+    }
+
+    /// @brief Sorts triangulations for each face
+    void sortTriangulations()
+    {
+        for (auto &triangulations : triangulation_per_face)
+        {
+            sort(triangulations.begin(), triangulations.end());
+        }
+    }
+
+    /// @brief Removes duplicated triangulations
+    void removeDuplicated()
+    {
+        vector<vector<pair<int, int>>> uniqueTriangulations;
+        int totalLength = allTriangulations[0].size();
+        for (auto &triangulation : allTriangulations)
+        {
+            set<pair<int, int>> allPairsInsideThisTriangulation;
+            bool arm = false;
+            bool selfloop = false;
+            for (auto &p : triangulation)
+            {
+                int a = min(p.first, p.second);
+                int b = max(p.first, p.second);
+                allPairsInsideThisTriangulation.insert({a, b});
+                if (present.find({a, b}) != present.end())
+                {
+                    arm = true;
+                    break;
+                }
+                if(a == b) {
+                    selfloop = true;
+                    break;
+                }
+            }
+            if (allPairsInsideThisTriangulation.size() != totalLength)
+            {
+                continue; // skip invalid triangulations
+            }
+            if (arm)
+            {
+                continue; // skip triangulations having edges not in present
+            }
+            if(selfloop) {
+                continue; // skip triangulations having self-loops
+            }
+            else
+            {
+                uniqueTriangulations.push_back(triangulation);
+            }
+        }
+        allTriangulations.clear();
+        for (auto triangulation : uniqueTriangulations)
+        {
+            allTriangulations.push_back(triangulation);
+        }
+    }
+
+    void refineTriangulations()
+    {
+        sortTriangulations();
+        vector<pair<int, int>> current;
+        combineTriangulations(triangulation_per_face, 0, current, allTriangulations);
+
+        // removeDuplicated();
+    }
+
+    void printTriangulationsPerFace()
+    {
+        int face_no = 0;
+        for (auto &face_triangulations : triangulation_per_face)
+        {
+            cout << "Face " << face_no << " has " << face_triangulations.size() << " triangulations:" << endl;
+            for (auto &triangulation : face_triangulations)
+            {
+                for (auto &chord : triangulation)
+                {
+                    cout << "(" << chord.first << ", " << chord.second << ") , ";
+                }
+                cout << endl;
+            }
+            face_no++;
+        }
+    }
+
+    void printAllTriangulations()
+    {
+        cout << "Total triangulations in triconnected component: " << allTriangulations.size() << endl;
+        for (auto &triangulation : allTriangulations)
+        {
+            for (auto &chord : triangulation)
+            {
+                cout << "(" << chord.first << ", " << chord.second << ") , ";
+            }
+            cout << endl;
+        }
+    }
+};
