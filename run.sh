4022ef5b065b7e75c6a297655324f7adfdb1c3df
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2026-09-25 14:46:42 +0600

    completed the run.sh file

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@
 ./build/bench_total --algo=oneconnected --cases=biconnected
 
 # Biconnected algorithm on everything
-./build/bench_total --algo=biconnected
+./build/bench_total --algo=biconnected --cases=biconnected
 
 # Per-triangulation timing, Oneconnected algorithm, all inputs, 5k limit
 ./build/bench_individual --algo=oneconnected --limit=5000
+
+# Per-triangulation timing, Oneconnected algorithm, all inputs, 5k limit
+./build/bench_individual --algo=biconnected --limit=5000
